f090ec56bc4905bb03ad876c4c7477410c8424e0
wonsunil <seonil6352@gmail.com>
2023-04-20 19:37:51 +0900

    commit

--- json/js/common.js
+++ json/js/common.js
@@ -1,87 +1,41 @@
 const $container = document.querySelector(".container");
-const $blur = document.querySelector(".blur");
-const $popup = document.querySelector(".popup");
-const $popupTitle = $popup.querySelector(".popupTitle");
-const $popupContent = $popup.querySelector(".popupContent");
-const $popupDate = $popup.querySelector(".popupDate");
-const $popupName = $popup.querySelector(".popupName");
-
-const state = {
-  currentIndex: 0,
-  maxIndex: 0,
-  limit: 20,
-};
-
-window.addEventListener("scroll", function () {
-  const { scrollY, innerHeight } = window;
-  const { scrollHeight } = document.body;
-  if (scrollY + innerHeight >= scrollHeight - 100) {
-    $main();
-  }
-});
 
 async function $main() {
   let posts = await fetch("./json/posts.json").then((res) => res.json());
   let members = await fetch("./json/members.json").then((res) => res.json());
-  let categorys = await fetch("./json/categorys.json").then((res) =>
-    res.json()
-  );
-  console.log(categorys);
-
-  const sortDate = posts.sort((a, b) => new Date(b.date) - new Date(a.date));
-
-  state.maxIndex = Math.ceil(posts.lenght / state.limit);
-
-  const { currentIndex, maxIndex, limit } = state;
-  if (currentIndex + 1 > maxIndex) return;
-  const startIndex = currentIndex * limit;
-  const endIndex = startIndex + limit;
-  posts = posts.slice(startIndex, endIndex);
+  let categorys = await fetch("./json/categorys.json").then((res) => res.json());
+  console.log(posts);
+  console.log(members);
 
   posts.forEach((Arr) => {
     const $box = document.createElement("div");
     const $imgBox = document.createElement("div");
     const $title = document.createElement("h4");
-    const $content = document.createElement("h5");
-    const $date = document.createElement("h5");
-    const $name = document.createElement("h5");
+    const $contents = document.createElement("h5");
+    const $name = document.createElement("p");
+    const $date = document.createElement("p");
     const memberIndex = Arr.memberIndex;
     const $memberName = members[memberIndex].name;
-
-    $box.addEventListener("click", function () {
-      $blur.style.display = "block";
-      $popup.style.display = "block";
-      $popupTitle.textContent = Arr.title;
-      $popupContent.textContent = Arr.contents;
-      $popupDate.textContent = Arr.date;
-      $popupName.innerHTML = `by <span>${$memberName}</span>`;
-    });
-
-    $blur.addEventListener("click", function () {
-      $blur.style.display = "none";
-      $popup.style.display = "none";
-    });
 
     $box.classList.add("box");
     $imgBox.classList.add("imgBox");
     $title.classList.add("title");
-    $content.classList.add("content");
-    $date.classList.add("date");
+    $contents.classList.add("contents");
     $name.classList.add("name");
+    $date.classList.add("date");
 
     $title.innerText = `${Arr.title}`;
-    $content.innerText = `${Arr.contents}`;
+    $contents.innerText = `${Arr.contents}`;
+    $name.innerHTML = `by ${$memberName}`;
     $date.innerText = `${Arr.date}`;
-    $name.innerHTML = `by <span>${$memberName}</span>`;
+    $box.innerHTML = `<hr>`;
 
     $container.append($box);
     $box.append($imgBox);
     $box.append($title);
-    $box.append($content);
-    $box.append($date);
+    $box.append($contents);
     $box.append($name);
+    $box.append($date);
   });
-  state.currentIndex++;
 }
-
 $main();
