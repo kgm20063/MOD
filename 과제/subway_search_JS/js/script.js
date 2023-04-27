a482d7443417985a56944eebb2ea5c72cee16528
kgm20063 <kgm20063@gmail.com>
2023-04-27 23:34:04 +0900

    주석 제거

--- 과제/subway_search_JS/js/script.js
+++ 과제/subway_search_JS/js/script.js
@@ -3,15 +3,6 @@ const $search_btn = document.querySelector("button");
 const $list = document.querySelector(".autocomplete > ul");
 const $result = document.querySelector(".result");
 
-// async function getData() {
-//   await stationList.data.forEach((e) => {
-//     const keys = Object.values(e);
-
-//   });
-// }
-// getData();
-
-// input
 $input.addEventListener("input", () => {
   $list.textContent = null; // 계속 추가 되는것을 막아줌
   const inputValue = $input.value;
@@ -19,20 +10,15 @@ $input.addEventListener("input", () => {
 
   stList.slice(0, 10).forEach((e) => {
     const li = document.createElement("li");
-    li.textContent = `${e.station_nm} ${e.line_num}`;
+    li.innerHTML = `${e.station_nm} ${e.line_num}`;
     $list.append(li);
   });
   if (inputValue == "") $list.textContent = null;
 });
 
-// button
 $search_btn.addEventListener("click", () => {
   const inputValue = $input.value;
   const stlist = stationList.data.filter((e) => e.station_nm.includes(inputValue));
-  // stlist.forEach((e) => {
-  //   e.station_cd;
-  // });
-  // timeList.data.forEach((e) => console.log(e.station_cd));
 
   stlist.forEach((list) => {
     list.textContent = null;
