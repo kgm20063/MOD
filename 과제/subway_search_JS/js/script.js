8c92a698844fa669875a2f1420c422bed9d20a46
kgm20063 <kgm20063@gmail.com>
2023-04-30 19:05:59 +0900

    init

--- 과제/subway_search_JS/js/script.js
+++ 과제/subway_search_JS/js/script.js
@@ -2,15 +2,28 @@ const $input = document.querySelector("input");
 const $search_btn = document.querySelector("button");
 const $list = document.querySelector(".autocomplete > ul");
 const $result = document.querySelector(".result");
+let nowindex = 0;
 
-$input.addEventListener("input", () => {
+$input.addEventListener("input", (e) => {
+  console.log(e.keycode);
   $list.textContent = null; // 계속 추가 되는것을 막아줌
   const inputValue = $input.value;
   const stlist = stationList.data.filter((e) => e.station_nm.includes(inputValue));
+  stlist.sort(function (a, b) {
+    let aa = a.station_nm;
+    let bb = b.station_nm;
+    if (aa < bb) {
+      return -1;
+    }
+    if (aa > bb) {
+      return 1;
+    }
+    return 0;
+  });
 
   stlist.slice(0, 10).forEach((e) => {
     const li = document.createElement("li");
-    const replaceText = e.station_nm.replace(inputValue, `<span class="red">${inputValue}</span>`); // 입력한 value값과 같은 문자를 span태그로 감싸게 하고 클래스 red를 넣음
+    const replaceText = e.station_nm.replace(inputValue, `<span class="yellow">${inputValue}</span>`); // 입력한 value값과 같은 문자를 span태그로 감싸게 하고 클래스 red를 넣음
     li.innerHTML = `${replaceText} ${e.line_num}`;
     $list.append(li);
   });
@@ -22,8 +35,8 @@ $search_btn.addEventListener("click", () => {
   const stlist = stationList.data.filter((e) => e.station_nm.includes(inputValue));
   $result.innerHTML = "";
   stlist.sort(function (a, b) {
-    const aa = a.station_nm;
-    const bb = b.station_nm;
+    let aa = a.station_nm;
+    let bb = b.station_nm;
     if (aa < bb) {
       return -1;
     }
